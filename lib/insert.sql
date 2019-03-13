9f1471b610e802aca33ccffd4d5fc74d8f5bd358
J. Singleton <jennifer.singleton@gmail.com>
2019-03-13 02:28:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("title", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("title 2", 2, 1);
 
@@ -7,12 +8,24 @@ INSERT INTO subgenres (name) VALUES ("apocalypse");
 INSERT INTO authors (name) VALUES ("Really Good Author");
 INSERT INTO authors (name) VALUES ("So-So Author");
 
+=======
+-- * 2 series
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("title", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("title 2", 2, 1);
+-- * 2 sub-genres
+INSERT INTO subgenres (name) VALUES ("space");
+INSERT INTO subgenres (name) VALUES ("apocalypse");
+-- * 2 authors
+INSERT INTO authors (name) VALUES ("Really Good Author");
+-- * 3 books in each series
+>>>>>>> 81fbbaab94e69edd16c40280ab70a839fb764187
 INSERT INTO books (title, year, series_id) VALUES ("Deep Spcae Travels", 1979, 1);
 INSERT INTO books (title, year, series_id) VALUES ("When the World Ends", 1950, 1);
 INSERT INTO books (title, year, series_id) VALUES ("Space Cowboys", 1981, 1);
 INSERT INTO books (title, year, series_id) VALUES ("Deep Spcae Travels", 1979, 2);
 INSERT INTO books (title, year, series_id) VALUES ("When the World Ends", 1950, 2);
 INSERT INTO books (title, year, series_id) VALUES ("Space Cowboys", 1981, 2);
+<<<<<<< HEAD
 
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 1", "motto 1", "species 1", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 2", "motto 2", "species 2", 1, 1);
@@ -23,6 +36,18 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("cha
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 7", "motto 7", "species 7", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 8", "motto 8", "species 8", 2, 2);
 
+=======
+-- * 8 characters
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 1", "motto 1", "species 1", 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 2", "motto 2", "species 2", 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 3", "motto 3", "species 3", 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 4", "motto 4", "species 4", 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 5", "motto 5", "species 5", 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 6", "motto 6", "species 6", 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 7", "motto 7", "species 7", 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("character 8", "motto 8", "species 8", 2);
+--   * 4 characters in each series
+>>>>>>> 81fbbaab94e69edd16c40280ab70a839fb764187
 INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
 INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
 INSERT INTO character_books (character_id, book_id) VALUES (1, 3);
@@ -40,3 +65,8 @@ INSERT INTO character_books (character_id, book_id) VALUES (6, 5);
 INSERT INTO character_books (character_id, book_id) VALUES (6, 6);
 INSERT INTO character_books (character_id, book_id) VALUES (7, 4);
 INSERT INTO character_books (character_id, book_id) VALUES (8, 5);
+<<<<<<< HEAD
+=======
+--     * of each of those 4, make 2 in all of the books in a series, and 2 in just 1 book in a series
+-- * Note you will need to insert values into your character_books join table
+>>>>>>> 81fbbaab94e69edd16c40280ab70a839fb764187
